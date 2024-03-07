b6e1a901e18de2180217ba671b19e46fbad332db
Arundas MK <mkarundas@gmail.com>
2024-03-07 20:51:26 +0530

    adding button component

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,9 +1,18 @@
+import GameBoard from "./components/Gameboard"
+import Player from "./components/Player"
+
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X"/>
+          <Player initialName="Player 2" symbol="0"/>
+        </ol>
+        <GameBoard/>
+      </div>
+    </main>
   )
 }
 
