0696b577aea8d494a237c5c93843253b783ce0b7
Arundas MK <mkarundas@gmail.com>
2024-03-08 21:02:10 +0530

    adding selection for buttons

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react"
+
 import GameBoard from "./components/Gameboard"
 import Player from "./components/Player"
 
 
 function App() {
+
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectActivePlayer() {
+    setActivePlayer((currActivePlayer)=> currActivePlayer === 'X' ? 'O' : 'X');
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X"/>
-          <Player initialName="Player 2" symbol="0"/>
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer == "X"}/>
+          <Player initialName="Player 2" symbol="0" isActive={activePlayer == "O"}/>
         </ol>
-        <GameBoard/>
+        <GameBoard onSelectSquare={handleSelectActivePlayer} activePlayerSymbol={activePlayer}/>
       </div>
     </main>
   )
